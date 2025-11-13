9a32395fa10807811a077194a1dce4fbf2d2d637
Reyad Hasan <reyadh418@gmail.com>
2025-11-13 17:01:42 +0600

    Added multiplication and refined code.

--- calculator.c
+++ calculator.c
@@ -2,11 +2,12 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main()
 {
-    int operation, result;
-    printf("It's a text based calculator, There is no graphics.\n");
+    int operation, result, back_to_menu;
+    printf("\n\nIt's a text based calculator, There is no graphics.\n");
 
     main_menu:
     {
@@ -17,28 +18,34 @@ int main()
             printf("What operation you want to do?\n");
 
             //Arithmatic Options
-            printf("\t1. Addition\n\t2. Subtraction\n\t3. Multiplication\n\t4. Division\n");
+            printf("\nArithmatic Operations:\n");
+            printf("\t1. Addition\n\t2. Subtraction\n\t3. Multiplication\n\t4. Division\n\t5. Root\n\t6. Square\n\t7. Remainder\n");
             //Trigonometry options
-            printf("\t5. Sine\n\t6. Cosine\n\t7. Tangent\n\t8. Cotangent\n\t9. Secent\n\t10. Cosecent\n");
+            printf("\nTrigonometric Operations:\n");
+            printf("\t8. Sine(sin)\n\t9. Cosine(cos)\n\t10. Tangent(tan)\n\t11. Cotangent(cot)\n\t12. Secent(sec)\n\t13. Cosecent(cosec)\n");
+            //Inverse Trigonometric options
+            printf("\nInverse Trigonometric Operations:\n");
+            printf("\t14. Inverse Sine(sin⁻¹)\n\t15. Inverse Cosine(cos⁻¹)\n\t16. Inverse Tangent(tan⁻¹)\n\t17. Inverse Cotangent(cot⁻¹)\n\t18. Inverse Secent(sec⁻¹)\n\t19. Inverse Cosecent(cosec⁻¹)\n");
             //Logarithmic options
-            printf("\t11. Log(10 base)\n\t12. Ln(e base Log)\n");
+            printf("\nLogarithmic Operations:\n");
+            printf("\t20. Log(10 base)\n\t21. Ln(e base Log)\n");
 
             printf("Choice:\t");
             scanf("%d", &operation);
         } while (operation < 1 || operation > 12);
         
 
-        if (operation == 1)
+        if (operation == 1) //Addition
         {
             int choice;
             result = 0;
             do
             {
-                printf("\nHow many numbers you want to add?(01 - 05)\n");
+                printf("\nHow many numbers you want to add?(01 - 10)\n");
                 printf("Choose:\t");
                 scanf("%d", &choice);
 
-            } while (choice < 1 || choice > 5);
+            } while (choice < 1 || choice > 10);
             
             int values[choice];
 
@@ -54,10 +61,15 @@ int main()
 
             printf("\nThe result is: %d", result);
 
-            goto main_menu;
-        }
+            printf("\n\nDo you want to go back to main menu? (1 for Yes / 2 for No):\t");
+            scanf("%d", &back_to_menu);
+            if (back_to_menu == 1)
+            {
+                goto main_menu;
+            }
+        }// Addition End
 
-        else if (operation == 2)
+        else if (operation == 2) //Subtraction
         {
             printf("\nYou can only subtract two numbers at a time.\n");
             int a, b;
@@ -78,7 +90,46 @@ int main()
             
             printf("\nYour result is: %d", result);
 
-            goto main_menu;
-        }
-    }
-}
+            printf("\n\nDo you want to go back to main menu? (1 for Yes / 2 for No):\t");
+            scanf("%d", &back_to_menu);
+            if (back_to_menu == 1)
+            {
+                goto main_menu;
+            }
+        }//Subtraction End
+
+        else if (operation == 3) //Multiplication
+        {
+            int choice;
+            result = 1;
+            do
+            {
+                printf("\nHow many numbers you want to multiply?(01 - 05)\n");
+                printf("Choose:\t");
+                scanf("%d", &choice);
+
+            } while (choice < 1 || choice > 5);
+            
+            int values[choice];
+
+            printf("\nEnter your values\n");
+
+            for(int i = 0; i < choice; i++)
+            {
+                printf("\nValues:\t");
+                scanf("%d", &values[i]);
+
+                result *= values[i];
+            }
+
+            printf("\nThe result is: %d", result);
+
+            printf("\n\nDo you want to go back to main menu? (1 for Yes / 2 for No):\t");
+            scanf("%d", &back_to_menu);
+            if (back_to_menu == 1)
+            {
+                goto main_menu;
+            }
+        }// Multiplication End
+    }// main_menu loop
+} //main
